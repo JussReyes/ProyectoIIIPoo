3b72be1d3333539e1e4a486feb86d3ddc1dd6554
Jus_Reyes_ <jusarm05@gmail.com>
2024-11-14 23:56:54 -0600

    base image

--- core/src/main/java/Proyecto/Juego/Main.java
+++ core/src/main/java/Proyecto/Juego/Main.java
@@ -1,20 +1,25 @@
 package Proyecto.Juego;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class Main extends Game {
     SpriteBatch batch;
     BitmapFont font;
+    int width, height;
     
     @Override
     public void create() {
         batch = new SpriteBatch();
         font = new BitmapFont();  // Use default font
+        width = Gdx.graphics.getWidth();
+        height = Gdx.graphics.getHeight();
         this.setScreen(new MainScreen(this));  // Set the first screen
     }
 
--- core/src/main/java/Proyecto/Juego/MainScreen.java
+++ core/src/main/java/Proyecto/Juego/MainScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -12,6 +13,9 @@ public class MainScreen implements Screen {
     private Main game;
     private SpriteBatch batch;
     private BitmapFont font;
+    private Texture mapa;
+    private int height;
+    private int width;
 
     public MainScreen(Main game) {
         this.game = game;
@@ -21,21 +25,21 @@ public class MainScreen implements Screen {
     public void show() {
         batch = game.batch;
         font = game.font;
+        mapa = new Texture( "mapa.png");
+        height = game.height;
+        width = game.width;
     }
 
     @Override
     public void render(float delta) {
         // Clear the screen with a color
+        Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         // Start drawing with SpriteBatch
         batch.begin();
         
-        // Draw some simple text in the center of the screen
-        font.setColor(Color.BLUE);
-        font.draw(batch, "Welcome to My Game!", Gdx.graphics.getWidth() / 2 - 20 / 2, 
-                  Gdx.graphics.getHeight() / 2);
-        
+        batch.draw(mapa, 0, 0, width, height);
         batch.end();
         
         // Handle screen transitions or input events (optional)
