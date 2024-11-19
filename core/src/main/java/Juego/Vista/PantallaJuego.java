04636b58d353b4b35fdfdd5e23523874e7539f75
Jus_Reyes_ <jusarm05@gmail.com>
2024-11-19 15:05:10 -0600

    basureros e isla en pantalla de juego

--- core/src/main/java/Juego/Vista/PantallaJuego.java
+++ core/src/main/java/Juego/Vista/PantallaJuego.java
@@ -1,7 +1,5 @@
 package Juego.Vista;
 
-import Juego.Modelo.EstadoNivel;
-import Juego.Modelo.Nivel;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
@@ -10,6 +8,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import java.util.ArrayList;
 
 public class PantallaJuego implements Screen {
 
@@ -23,19 +22,54 @@ public class PantallaJuego implements Screen {
     private Sprite OceanoBack;
     
     float XBOceano = 0;
-    float YBOceano = 0;
+    float YBOceano = -70;
     
     private final Texture imgOceanoMid = new Texture( "OceanMiddle.png");
     private Sprite OceanoMid;
     
     float XMOceano = 0;
-    float YMOceano = 0;
+    float YMOceano = -55;
     
     private final Texture imgOceanoFront = new Texture( "OceanFront.png");
     private Sprite OceanoFront;
     
     float XFOceano = 0;
-    float YFOceano = 0;
+    float YFOceano = -20;
+    
+    private final Texture imgIsla = new Texture ("Island.png");
+    private Sprite isla;
+    
+    private final Texture imgPausa = new Texture ("pause.png");
+    private Sprite pausa;
+    
+    private final Texture imgPlastico = new Texture("PlasticCan.png");
+    private Sprite plastico;
+    
+    private final Texture imgVidrio = new Texture("GlassCan.png");
+    private Sprite vidrio;
+    
+    private final Texture imgMetal = new Texture("MetalCan.png");
+    private Sprite metal;
+    
+    private final Texture imgOrganico = new Texture("OrganicCan.png");
+    private Sprite organico;
+    
+    private final Texture imgBiologico = new Texture("BiologicCan.png");
+    private Sprite biologico;
+    
+    private final Texture imgPapel = new Texture("PaperCan.png");
+    private Sprite papel;
+    
+    private final Texture imgGeneral = new Texture("GeneralCan.png");
+    private Sprite general;
+    
+    private ArrayList<Sprite> basureros = new ArrayList<>();
+    
+    private float widthArriba;
+    private float yArriba;
+    
+    private float widthAbajo;
+    private float yAbajo;
     
     public PantallaJuego(Main game) {
         this.game = game;
@@ -50,6 +84,31 @@ public class PantallaJuego implements Screen {
         OceanoBack = new Sprite(imgOceanoBack);
         OceanoMid = new Sprite(imgOceanoMid);
         OceanoFront = new Sprite(imgOceanoFront);
+        
+        isla = new Sprite(imgIsla);
+        
+        papel = new Sprite(imgPapel);
+        basureros.add(papel);
+        
+        vidrio = new Sprite(imgVidrio);
+        basureros.add(vidrio);
+        
+        plastico = new Sprite(imgPlastico);
+        basureros.add(plastico);
+        
+        organico = new Sprite(imgOrganico);
+        basureros.add(organico);
+        
+        biologico = new Sprite(imgBiologico);
+        basureros.add(biologico);
+        
+        metal = new Sprite(imgMetal);
+        basureros.add(metal);
+        
+        general = new Sprite(imgGeneral);
+        basureros.add(general);
+        
+        pausa = new Sprite(imgPausa);
     }
 
     @Override
@@ -72,6 +131,36 @@ public class PantallaJuego implements Screen {
         XFOceano =  (XFOceano - 2) % 1512;
         OceanoFront.setPosition(XFOceano, YFOceano);
         OceanoFront.draw(batch);
+        
+        isla.setPosition(0, 0);
+        isla.draw(batch);
+        
+        pausa.setPosition(110, 573);
+        pausa.draw(batch);
+        
+        if (basureros.size() < 4) {
+            widthArriba = 121 * basureros.size() - 10;
+            yArriba = 412;
+        }
+        else{
+            widthArriba = 353;
+            yArriba = 500;
+            
+            widthAbajo = 121  * (basureros.size() - 3) - 10;
+            yAbajo = 325;
+        }
+        
+        for (int i = 0; i < basureros.size(); i++) {
+            Sprite basurero = basureros.get(i);
+            
+            if (i < 3){
+                basurero.setPosition(444 + 121 * i  + (355 -widthArriba) / 2, yArriba);
+            }
+            else{
+                basurero.setPosition(444 + 121*(i -3) + (355 - widthAbajo) / 2, yAbajo);
+            }
+            basurero.draw(batch);
+        }
         
         batch.end();
         
@@ -109,5 +198,9 @@ public class PantallaJuego implements Screen {
         batch.dispose();
         font.dispose();
         imgOceanoBack.dispose();
+        imgOceanoMid.dispose();
+        imgOceanoFront.dispose();
+        imgIsla.dispose();
+        imgPausa.dispose();
     }
 }
