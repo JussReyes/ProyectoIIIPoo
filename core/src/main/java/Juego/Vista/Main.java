38681503833b23fdad182ed362fcbdb0d714c00e
Jus_Reyes_ <jusarm05@gmail.com>
2024-11-25 15:38:45 -0600

    caja de texto más bonita

--- core/src/main/java/Juego/Vista/Main.java
+++ core/src/main/java/Juego/Vista/Main.java
@@ -20,7 +20,7 @@ public class Main extends Game {
         width = Gdx.graphics.getWidth();
         height = Gdx.graphics.getHeight();
         camara = new OrthographicCamera(width, height);
-        this.setScreen(new Login(this));  // Set the first screen
+        this.setScreen(new Recomendaciones(this));  // Set the first screen
     }
 
     @Override
