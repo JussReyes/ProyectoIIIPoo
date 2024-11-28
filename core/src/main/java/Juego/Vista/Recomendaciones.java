64179e88a38ec02bf67e844d36ee76dd56dc5ef7
XandersEG <166416633+XandersEG@users.noreply.github.com>
2024-11-27 19:56:39 -0600

    salir

--- core/src/main/java/Juego/Vista/Recomendaciones.java
+++ core/src/main/java/Juego/Vista/Recomendaciones.java
@@ -148,6 +148,10 @@ public class Recomendaciones implements Screen, Fuentes {
                 
                 
                 try {
+                    
+
+                    if (nom.isBlank())
+                     throw new IllegalArgumentException("        "+"Ingrese el nombre");
                     if (descomposición.getText().isBlank())
                         throw new IllegalArgumentException("Ingrese el número de días");
                     try{
@@ -156,9 +160,6 @@ public class Recomendaciones implements Screen, Fuentes {
                     catch (NumberFormatException ex) {
                         throw new IllegalArgumentException("Ingrese un número de días para el tiempo de descomposición");
                     }
-
-                    if (nom.isBlank())
-                     throw new IllegalArgumentException("        "+"Ingrese el nombre");
                     if (desc.isBlank())
                         throw new IllegalArgumentException("      Ingrese la descripción");
                     if (ruta.isBlank())
@@ -185,7 +186,8 @@ public class Recomendaciones implements Screen, Fuentes {
             
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                    game.setScreen(new Mapa(game, controlador));
+                dispose();
+                game.setScreen(new Mapa(game, controlador));
             }
         });
         
@@ -197,6 +199,7 @@ public class Recomendaciones implements Screen, Fuentes {
         stage.addActor(descripcion);
         stage.addActor(recomendaciones);
         stage.addActor(enviar);
+        stage.addActor(salir);
         
     }
         
